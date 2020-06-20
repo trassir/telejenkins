c784eeb541f48edb46729f5bcb92d4aa0c942b2f
Artalus <artalus-mail@yandex.ru>
2020-06-20 17:59:15 +0300

    create PostNotify from json arguments

--- jenkinsdssl/post.py
+++ jenkinsdssl/post.py
@@ -1,3 +1,54 @@
+import re
+
+def error(x: str):
+    raise ValueError(x)
+
+RE_SEPARATORS = re.compile(r'[,;]')
+def decode_notify(s: str):
+    return re.sub(RE_SEPARATORS, ' ', s).split()
+
+class PayloadUrlButton:
+    def __init__(self, url, name):
+        super().__init__()
+        self.url = url
+        self.name = name
+
 class PostNotify:
-    def __init__(self, *args, **kwargs):
-        pass
+    def arg(self, name, typename, nonemessage=None, optional=False, args=None):
+        _args = args or self.kwargs
+        value = _args.get(name)
+        if not value:
+            if optional:
+                return None
+            else:
+                raise ValueError(nonemessage)
+        if not isinstance(value, typename):
+            raise ValueError(f'"{name}" must be a {typename} (got a {type(value)})')
+        return value
+
+    def __init__(self, **kwargs):
+        self.kwargs = kwargs
+        notify = self.arg('notify', str, 'Parameters should include "notify"')
+        self.notify = decode_notify(notify)
+        self.type = self.arg('type', str, 'Parameters should include "type"')
+        if self.type == 'simple':
+            self.build_url    = self.arg('url',      str, '"simple" type supposed to include "url"')
+            self.job_name     = self.arg('job_name', str, '"simple" type supposed to include "job_name"')
+            self.build_result = self.arg('status',   str, '"simple" type supposed to include "status"')
+            self.message      = self.arg('text',     str, optional=True) or ''
+        elif self.type == 'markdown':
+            self.markdown = self.arg('markdown', str, '"markdown" type supposed to include "markdown"')
+        else:
+            raise ValueError(f'Unknown request type "{self.type}"')
+        payload = self.arg('payload', list, optional=True)
+        self.payload = []
+        for p in payload:
+            tp = self.arg('type', str, 'Payload should include "type"', args=p)
+            if tp == 'url_button':
+                url  = self.arg('url', str, '"url_button" payload supposed to include "url"', args=p)
+                name = self.arg('name', str, '"url_button" payload supposed to include "name"', args=p)
+                # self.payload.append(dict(type=tp, url=url, name=name))
+                self.payload.append(PayloadUrlButton(url, name))
+            else:
+                raise ValueError(f'Unknown payload type "{tp}"')
+        del self.kwargs
--- jenkinsdssl/tornado.py
+++ jenkinsdssl/tornado.py
@@ -1,42 +1,53 @@
-
+import json
 import logging
 from threading import Thread
 
 from telegram.ext import Updater
-import tornado.web
-from tornado.escape import json_decode
-from tornado.web import url, RequestHandler
+import tornado.web as tw
 from telegram.utils.webhookhandler import WebhookServer
 
 from jenkinsdssl.post import PostNotify
 
 logger = logging.getLogger(__name__)
 
-class MainHandler(RequestHandler):
+class MainHandler(tw.RequestHandler):
     def initialize(self, upd: Updater):
         self.upd = upd
 
     def prepare(self):
-        self.args = json_decode(self.request.body) if self.request.body else dict()
+        self.args = json.loads(self.request.body) if self.request.body else dict()
         logger.info(f'POST prepared with args {self.args}')
 
     def post(self):
-        self.upd.update_queue.put(PostNotify(self.args))
-        logger.info(f'POST sent {self.args}')
+        self.set_header('content-type', 'application/json')
+        try:
+            try:
+                p = PostNotify(**self.args)
+            except ValueError as e:
+                logger.info(f'Could not construct PostNotify: {e}')
+                self.set_status(400)
+                self.finish(json.dumps(dict(error=f'Bad arguments: {e}')))
+                return
+            self.upd.update_queue.put(p)
+            logger.info(f'POST data enqueued as {json.dumps(p, default=lambda x: x.__dict__)}')
+        except Exception as e:
+            logger.error(e)
+            self.set_status(500)
+            self.finish(json.dumps(dict(error=f'Some internal server error: {e}')))
 
 
 class WebhookThread(Thread):
     def __init__(self, upd: Updater):
         super().__init__()
-        app = tornado.web.Application([
-            url(r"/notify", MainHandler, dict(upd=upd)),
+        app = tw.Application([
+            tw.url(r"/notify", MainHandler, dict(upd=upd)),
         ])
         port = 8888
         host = "0.0.0.0"
         self.webhooks = WebhookServer(host, port, app, None)
         logger.info(f'Tornado app created @ {host}:{port}')
 
-    def run(self) -> None:
+    def run(self):
         logger.info(f'Tornado serving forever')
         self.webhooks.serve_forever()
 
