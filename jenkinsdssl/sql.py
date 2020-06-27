be0d70abaee59f24e67fc863bc8111b33de311d3
Artalus <artalus-mail@yandex.ru>
2020-06-27 12:52:00 +0300

    replace raw psycopg2 with sqlalchemy

--- jenkinsdssl/sql.py
+++ jenkinsdssl/sql.py
@@ -1,21 +1,38 @@
-import psycopg2
+import logging
+import sqlalchemy as db
+
+logger = logging.getLogger(__name__)
 
 class sql:
-    _db_connection = None
-    _db_cursor = None
+    _connection = None
+
+    chats = None
+    aliases = None
     @staticmethod
     def init(**kwargs):
-        sql._db_connection = psycopg2.connect(**kwargs)
-        sql._db_cursor = sql._db_connection.cursor()
+        from collections import namedtuple
+        S = namedtuple('S', 'dbname user host password port')
+        d = S(**kwargs)
+
+        engine = db.create_engine(f'postgres://{d.user}:{d.password}@{d.host}:{d.port}/{d.dbname}')
+        metadata = db.MetaData()
+        sql._connection = engine.connect()
+
+        sql.chats = db.Table('chats', metadata,
+            db.Column('id', db.Integer(), primary_key=True),
+            db.Column('description', db.VARCHAR()),
+        )
+        sql.aliases = db.Table('aliases', metadata,
+            db.Column('chat_id', db.Integer(), db.ForeignKey("chats.id"),),
+            db.Column('alias', db.VARCHAR(), primary_key=True),
+        )
+        metadata.create_all(engine) #Creates the table
+
     @staticmethod
-    def set(*args, **kwargs):
-        try:
-            sql._db_cursor.execute(*args, **kwargs)
-            sql._db_connection.commit()
-        except:
-            sql._db_connection.rollback()
-            raise
+    def get(query):
+        logger.info(f'.get:\n{query}')
+        return sql._connection.execute(query).fetchall()
     @staticmethod
-    def get(*args, **kwargs):
-        sql._db_cursor.execute(*args, **kwargs)
-        return sql._db_cursor.fetchall()
+    def set(query):
+        logger.info(f'.set:\n{query}')
+        return sql._connection.execute(query)
